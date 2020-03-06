05d154af44e07354c7b6b429472831a4e8ae1b1e
Martin Meyer <martin.meyer@inftec.ch>
2020-03-06 17:36:38 +0100

    fireauth: Google Authentication

--- src/app/navigation/account-button/account-button.component.ts
+++ src/app/navigation/account-button/account-button.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from "@angular/core";
+import { AngularFireAuth } from "@angular/fire/auth";
+import { auth } from "firebase/app";
+import { Observable } from "rxjs";
 
 @Component({
-  selector: 'app-account-button',
-  templateUrl: './account-button.component.html',
-  styleUrls: ['./account-button.component.scss']
+  selector: "app-account-button",
+  templateUrl: "./account-button.component.html",
+  styleUrls: ["./account-button.component.scss"],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AccountButtonComponent implements OnInit {
+export class AccountButtonComponent {
+  user$: Observable<firebase.User> = this.fireAuth.user;
 
-  constructor() { }
+  constructor(private fireAuth: AngularFireAuth) {}
 
-  ngOnInit(): void {
+  loginGoogle() {
+    this.fireAuth.signInWithPopup(new auth.GoogleAuthProvider());
   }
 
+  logout() {
+    this.fireAuth.signOut();
+  }
 }
