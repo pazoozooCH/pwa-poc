fecc746ac80d89ef0e4071779672386f5e2261ad
Martin Meyer <martin.meyer@inftec.ch>
2020-03-10 21:47:40 +0100

    drag-and-drop: Reordering and dragging to other list

--- src/app/demo/demo.module.ts
+++ src/app/demo/demo.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { DragDropModule } from "@angular/cdk/drag-drop";
 import { TechieDiariesListComponent } from "./techie-diaries-list/techie-diaries-list.component";
 import { DemoRoutingModule } from "./demo-routing.module";
 import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
 import { MatCardModule } from "@angular/material/card";
-import { DragAndDropComponent } from './drag-and-drop/drag-and-drop.component';
+import { DragAndDropComponent } from "./drag-and-drop/drag-and-drop.component";
 
 @NgModule({
   declarations: [TechieDiariesListComponent, DragAndDropComponent],
   imports: [
     DemoRoutingModule,
     CommonModule,
+    DragDropModule,
     MatCardModule,
     MatProgressSpinnerModule
   ]
--- src/app/demo/drag-and-drop/drag-and-drop.component.ts
+++ src/app/demo/drag-and-drop/drag-and-drop.component.ts
@@ -1,15 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem
+} from "@angular/cdk/drag-drop";
+
+interface ListItem {
+  originalIndex: number;
+  value: string;
+}
 
 @Component({
-  selector: 'app-drag-and-drop',
-  templateUrl: './drag-and-drop.component.html',
-  styleUrls: ['./drag-and-drop.component.scss']
+  selector: "app-drag-and-drop",
+  templateUrl: "./drag-and-drop.component.html",
+  styleUrls: ["./drag-and-drop.component.scss"]
 })
 export class DragAndDropComponent implements OnInit {
+  lists = [
+    { name: "Some List", list: DragAndDropComponent.createList() },
+    {
+      name: "Another List",
+      list: DragAndDropComponent.createList("Another Card", 15)
+    }
+  ];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  private static createList(prefix = "Card", length = 10): ListItem[] {
+    return Array.from(
+      Array(length),
+      (e, i) =>
+        ({
+          originalIndex: i,
+          value: `${prefix} ${i + 1}`
+        } as ListItem)
+    );
   }
 
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
+  }
 }
