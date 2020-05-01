3e88912fa6f316211cac01ea967f4938170a6ed3
Martin Meyer <martin.meyer@inftec.ch>
2020-05-01 17:34:34 +0200

    animations: Added list filtering

--- src/app/demo/animation/enter-leave-animation/enter-leave-animation.component.ts
+++ src/app/demo/animation/enter-leave-animation/enter-leave-animation.component.ts
@@ -14,7 +14,7 @@ interface ListItem {
     trigger("myInsertRemoveTrigger", [
       transition(":enter", [
         style({ opacity: 0, height: "0px" }),
-        animate("300ms", style({ opacity: 1, height: "*" }))
+        animate("200ms", style({ opacity: 1, height: "*" }))
       ]),
       transition(":leave", [
         animate("200ms", style({ opacity: 0, height: "0px" }))
@@ -25,28 +25,50 @@ interface ListItem {
 export class EnterLeaveAnimationComponent {
   static readonly initialItems = 4;
 
-  items: ListItem[] = [];
+  private items: ListItem[] = [];
+  filtered = false;
+  filteredItems: ListItem[] = [];
 
   private index = 0;
 
   constructor() {
     for (let i = 0; i < EnterLeaveAnimationComponent.initialItems; i++) {
-      this.addItem();
+      this.addItem(false);
     }
+    this.filter();
   }
 
-  addItem() {
+  addItem(refilter = true) {
     this.items.push({
       text: `Item ${this.index++}`,
       color: this.generateColor()
     });
+
+    if (refilter) {
+      this.filter();
+    }
   }
 
-  removeItem(index: number) {
+  removeItem(item) {
+    const index = this.items.indexOf(item);
     this.items.splice(index, 1);
+    this.filter();
+  }
+
+  toggleFilter() {
+    this.filtered = !this.filtered;
+    this.filter();
   }
 
   private generateColor() {
     return "hsl(" + Math.floor(Math.random() * 361) + ",50%,75%)";
+  }
+
+  private filter() {
+    if (this.filtered) {
+      this.filteredItems = this.items.filter((_, index) => index % 2 === 0);
+    } else {
+      this.filteredItems = this.items;
+    }
   }
 }
